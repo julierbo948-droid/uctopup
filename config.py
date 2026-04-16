cfd95244dba3443ca0b70a2ee1186d885c4783ed
julierbo948-droid <julierbo948@gmail.com>
2026-04-17 01:06:08 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+GOOGLE_EMAIL = os.getenv("GOOGLE_EMAIL")
+GOOGLE_PASS = os.getenv("GOOGLE_PASS")
+
+# Smile One PUBG Product ID
+PUBG_PRODUCT_ID = "650"
