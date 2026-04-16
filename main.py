0cf4d420702fd671252a8f807b929370097649b6
julierbo948-droid <julierbo948@gmail.com>
2026-04-17 02:20:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, F
 from config import BOT_TOKEN
-from handlers import start_handler, buy_handler, set_cookie_handler
-
+from handlers import start_handler, buy_handler, set_cookie_handler, topup_handler # topup_handler ထပ်ဖြည့်ပါ
 # Logging ကို သတ်မှတ်ခြင်း
 logging.basicConfig(level=logging.INFO)
 
@@ -23,6 +22,8 @@ async def main():
 
     # ၃။ UC ဝယ်ယူသည့် Command (.buy သို့မဟုတ် /buy)
     dp.message.register(buy_handler, lambda m: m.text and m.text.lower().startswith((".", "/")) and "buy" in m.text.lower())
+
+    dp.message.register(topup_handler, F.text.regexp(r"(?i)^\.topup\s+([a-zA-Z0-9]+)"))
 
     # Bot စတင်လည်ပတ်ကြောင်း အကြောင်းကြားစာ
     print("🚀 PUBG Voucher Bot is running...")
